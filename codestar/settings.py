8a1b389066aec4f6c0b540bd439091ced738ac71
Daniel Turner <lunartechfreek@googlemail.com>
2024-05-30 19:04:14 +0000

    Add database model

--- codestar/settings.py
+++ codestar/settings.py
@@ -92,6 +92,11 @@ DATABASES = {
 }
 
 
+CSRF_TRUSTED_ORIGINS = [
+    "https://*.gitpod.io",
+    "https://*.herokuapp.com"
+]
+
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
