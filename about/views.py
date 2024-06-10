3f504ae20dae525c8fc493443a7c4fda4e2e0199
Daniel Turner <lunartechfreek@googlemail.com>
2024-06-10 18:49:36 +0000

    Complete tidying up lesson

--- about/views.py
+++ about/views.py
@@ -6,7 +6,16 @@ from .forms import CollaborateForm
 # Create your views here.
 def about_me(request):
     """
-    Renders the About page
+    Renders the most recent information on the website author
+    and allows user collaboration requests
+    Displays an individual instance of :model:`about.About`.
+    **Context**
+    ``about``
+        The most recent instance of :model:`about.About`.
+    ``collaborate_form``
+        An instance of :form:`about.CollaborationForm`.
+    **Template**
+    :template:`about/about.html`
     """
     about = About.objects.all().order_by('-updated_on').first()
 
